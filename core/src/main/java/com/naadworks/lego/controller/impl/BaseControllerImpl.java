cdf0945292f5f52b447e56dffb731050f5e70ec9
Nancy Nigam <nancynigam94@gmail.com>
2019-04-02 18:44:16 +0530

    Abstraction added to BaseController and BaseControllerImpl

--- core/src/main/java/com/naadworks/lego/controller/impl/BaseControllerImpl.java
+++ core/src/main/java/com/naadworks/lego/controller/impl/BaseControllerImpl.java
@@ -1,11 +1,14 @@
 package com.naadworks.lego.controller.impl;
 
 import com.naadworks.lego.Status;
+import com.naadworks.lego.controller.BaseController;
 import com.naadworks.lego.entity.BaseEntity;
 import com.naadworks.lego.entry.BaseEntry;
 import com.naadworks.lego.exceptions.BaseException;
 import com.naadworks.lego.service.impl.BaseServiceImpl;
 import com.naadworks.lego.view.BaseView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import java.util.ArrayList;
@@ -14,25 +17,17 @@ import java.util.List;
 import static com.naadworks.lego.enums.StatusType.ERROR;
 import static com.naadworks.lego.enums.StatusType.SUCCESS;
 
-public abstract class BaseControllerImpl<V extends BaseView, T extends BaseEntry, E extends BaseEntity> {
+public abstract class BaseControllerImpl<V extends BaseView, T extends BaseEntry, E extends BaseEntity, ID> implements BaseController<T,E,V,ID> {
+    protected Logger LOGGER = LoggerFactory.getLogger(this.getClass().getName());
+    private BaseServiceImpl<T, E, ID> service;
 
-    private BaseServiceImpl<T, E> service;
-
-    public BaseServiceImpl<T, E> getService() {
-        return service;
-    }
-
-    public void setService(BaseServiceImpl<T, E> service) {
-        this.service = service;
-    }
-
-    public V findById(String id) {
+    public V findById(ID id) {
         V view = this.createResponse(null);
         Status status = new Status(SUCCESS, "Operation is successful");
         List<T> data = new ArrayList<>();
 
         try {
-            T entryFetched = this.getService().getById(id);
+            T entryFetched = this.getService().findById(id);
             data.add(entryFetched);
             view = this.createResponse(data);
         }
@@ -42,6 +37,47 @@ public abstract class BaseControllerImpl<V extends BaseView, T extends BaseEntry
         view.setStatus(status);
         return view;
 
+    }
+
+    public V create(T t) {
+        V view = this.createResponse(null);
+        Status status = new Status(SUCCESS, "Operation is successful");
+        List<T> data = new ArrayList<>();
+        try {
+            T entryCreated = this.getService().create(t);
+            data.add(entryCreated);
+            view = this.createResponse(data);
+        }
+        catch (BaseException be) {
+            status.setType(ERROR);
+        }
+        view.setStatus(status);
+        return view;
+    }
+
+    public V update(ID id, T t) {
+        V view = this.createResponse(null);
+        Status status = new Status(SUCCESS, "Operation is successful");
+        List<T> data = new ArrayList<>();
+        try {
+            T entryUpdate = this.getService().update(id, t);
+            data.add(entryUpdate);
+            view = this.createResponse(data);
+        }
+        catch(BaseException be) {
+            status.setType(ERROR);
+        }
+        view.setStatus(status);
+        return view;
+
+    }
+
+    public BaseServiceImpl<T, E, ID> getService() {
+        return service;
+    }
+
+    public void setService(BaseServiceImpl<T, E, ID> service) {
+        this.service = service;
     }
 
     public abstract V createResponse(List<T> var1);
